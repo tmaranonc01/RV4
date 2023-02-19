405c2fe25d9df793b8bd7662255ec235bbb735fa
tmaranonc01 <dam08.2022@gmail.com>
2023-02-19 21:49:27 +0100

    Funciona

--- src/main/java/com/example/rv4/Desguace/Respository/DesguaceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.rv4.Desguace.Respository;
-
-import com.example.rv4.Coche.Domain.Coche;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DesguaceRepository extends JpaRepository<Coche, Long> {
-}
